9f8a72f82ab459ea5480faecbf1aff679b7b7cd8
VasilievVv <vvv141188@gmail.com>
2023-06-15 20:35:36 +0200

    load data for csv

--- backend/foodgram_backend/recipes/management/commands/load_ingredient_csv.py
+++ backend/foodgram_backend/recipes/management/commands/load_ingredient_csv.py
@@ -1,12 +1,31 @@
 import csv
 
-from django.conf import settings
-from django.core.management import BaseCommand
+from django.core.management.base import BaseCommand
+
+from foodgram_backend.settings import BASE_DIR
+from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
-    help = './'
+    help = 'Команда загрузки ингредиентов и их количества из csv'
 
-    def handle(self, *args, **options):
-        path = settings.BASE_DIR /
+    def handle(self, *args, **optoins):
+        self.stdout.write('Идет загрузка индгридиентов в бд')
+        Ingredient.objects.all().delete()
+        with open(
+                f'{BASE_DIR}/data/ingredients.csv',
+                encoding='utf-8',
+        ) as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                name = row[0]
+                measurement_unit = row[1]
+                Ingredient.objects.get_or_create(
+                    name=name,
+                    measurement_unit=measurement_unit
+                )
+        self.stdout.write('Загрузка завершена!')
+
+
 
--- backend/foodgram_backend/users/urls.py
+++ backend/foodgram_backend/users/urls.py
@@ -3,12 +3,12 @@ from rest_framework.routers import DefaultRouter
 
 from .views import FollowViewSet
 
-router_api_v1 = DefaultRouter()
-router_api_v1.register('follow', FollowViewSet)
+# router_api_v1 = DefaultRouter()
+# router_api_v1.register('', FollowViewSet)
 
 urlpatterns = [
     path('', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
-    path('users/', include(router_api_v1.urls)),
+    # path('auth/', include('djoser.urls.authtoken')),
+    path('users/', FollowViewSet.as_view({'get': 'subscriptions'})),
 
 ]
