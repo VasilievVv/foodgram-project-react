05cf9039be3ce143f5b6dbffcaa1fbf60978879e
VasilievVv <vvv141188@gmail.com>
2023-06-18 13:59:57 +0200

    follow viewset.mixins up

--- backend/foodgram_backend/users/urls.py
+++ backend/foodgram_backend/users/urls.py
@@ -8,8 +8,6 @@ router_api_v1.register('users', FollowViewSet, basename='follow')
 
 urlpatterns = [
     path('', include(router_api_v1.urls)),
-    # path('users/subscriptions/', FollowlistView.as_view()),
-    # path('users/<int:pk>/subscribe/', FollowCreateView.as_view()),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
--- backend/foodgram_backend/users/views.py
+++ backend/foodgram_backend/users/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 
-from rest_framework import status, views, viewsets
+from rest_framework import status,viewsets, mixins
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -11,39 +11,10 @@ from .serializers import FollowSerializer
 User = get_user_model()
 
 
-# class FollowlistView(views.APIView):
-#
-#     def get(self, request):
-#         following = User.objects.filter(following__user=self.request.user)
-#         serializer = FollowSerializer(
-#             following,
-#             many=True,
-#             context={'request': request})
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-#
-#
-# class FollowCreateView(views.APIView):
-#     """./"""
-#
-#     def post(self, request, pk):
-#         following = get_object_or_404(User, id=pk)
-#         serializer = FollowSerializer(
-#             following,
-#             data=request.data,
-#             context={'request': request})
-#         if serializer.is_valid():
-#             Follow.objects.create(user=request.user, following=following)
-#             return Response(serializer.data,
-#                             status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     def delete(self, request, pk):
-#         following = get_object_or_404(User, id=pk)
-#         get_object_or_404(Follow, user=request.user, following=following).delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class FollowViewSet(viewsets.ModelViewSet):
+class FollowViewSet(mixins.ListModelMixin,
+                    mixins.CreateModelMixin,
+                    mixins.DestroyModelMixin,
+                    viewsets.GenericViewSet):
     """>./"""
 
     @action(methods=['get'],
