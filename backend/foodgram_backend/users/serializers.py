31a716e43bf7ed7af12e84569dc02eaaebe4fd3b
VasilievVv <vvv141188@gmail.com>
2023-06-18 12:40:15 +0200

    follow serializer up

--- backend/foodgram_backend/users/serializers.py
+++ backend/foodgram_backend/users/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from django.contrib.auth import get_user_model
 
+from recipes.models import Recipe
 from .models import Follow
 
 User = get_user_model()
@@ -33,10 +34,19 @@ class UsersSerializer(serializers.ModelSerializer):
         return False
 
 
+class FollowRecipeSerializer(serializers.ModelSerializer):
+    """Сериализатор рецепта для подписок."""
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'name', 'cooking_time', ) #image добавить
+
 class FollowListSerializer(serializers.ModelSerializer):
     """"Сериализатор выаода инфы о подписке."""
 
     is_subscribed = serializers.SerializerMethodField()
+    recipes = FollowRecipeSerializer(read_only=True, many=True)
+    recipes_count = serializers.SerializerMethodField()
 
     class Meta:
         model = User
@@ -47,9 +57,14 @@ class FollowListSerializer(serializers.ModelSerializer):
             'first_name',
             'last_name',
             'is_subscribed',
-            # 'recipes',
-            # 'recipes_count', пока хз
+            'recipes',
+            'recipes_count',
         )
 
     def get_is_subscribed(self, value):
         return True
+
+    def get_recipes_count(self, value):
+        request = self.context.get('request')
+        author = request.user
+        author.recipes.count() #NULL ?
