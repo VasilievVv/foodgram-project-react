67a90351860eb38f9674382830bfc15a57b65194
VasilievVv <vvv141188@gmail.com>
2023-06-18 14:16:29 +0200

    follow refused to use router and viewset

--- backend/foodgram_backend/users/urls.py
+++ backend/foodgram_backend/users/urls.py
@@ -1,13 +1,10 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
 
-from .views import FollowViewSet
-
-router_api_v1 = DefaultRouter()
-router_api_v1.register('users', FollowViewSet, basename='follow')
+from .views import FollowlistView, FollowCreateView
 
 urlpatterns = [
-    path('', include(router_api_v1.urls)),
+    path('users/subscriptions/', FollowlistView.as_view()),
+    path('users/<int:pk>/subscribe/', FollowCreateView.as_view()),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
--- backend/foodgram_backend/users/views.py
+++ backend/foodgram_backend/users/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 
-from rest_framework import status,viewsets, mixins
+from rest_framework import status, views, generics
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -11,35 +11,68 @@ from .serializers import FollowSerializer
 User = get_user_model()
 
 
-class FollowViewSet(mixins.ListModelMixin,
-                    mixins.CreateModelMixin,
-                    mixins.DestroyModelMixin,
-                    viewsets.GenericViewSet):
-    """>./"""
+class FollowlistView(generics.ListAPIView):
 
-    @action(methods=['get'],
-            detail=False, )
-    def subscriptions(self, request):
-        """...0"""
-
+    def get(self, request):
         following = User.objects.filter(following__user=self.request.user)
-        serializer = FollowSerializer(following, many=True, context={'request': request})
+        serializer = FollowSerializer(
+            following,
+            many=True,
+            context={'request': request})
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @action(methods=['post', 'delete'], detail=True)
-    def subscribe(self, request, pk):
-        """../"""
 
+class FollowCreateView(generics.CreateAPIView,
+                       generics.DestroyAPIView):
+    """./"""
+
+    def post(self, request, pk):
         following = get_object_or_404(User, id=pk)
-        if request.method == 'POST':
-            serializer = FollowSerializer(following, data=request.data, context={'request': request})
-            if serializer.is_valid():
-                Follow.objects.create(user=request.user, following=following)
-                return Response(serializer.data,
-                                status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = FollowSerializer(
+            following,
+            data=request.data,
+            context={'request': request})
+        if serializer.is_valid():
+            Follow.objects.create(user=request.user, following=following)
+            return Response(serializer.data,
+                            status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def delete(self, request, pk):
+        following = get_object_or_404(User, id=pk)
         get_object_or_404(Follow, user=request.user, following=following).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# class FollowViewSet(mixins.ListModelMixin,
+#                     mixins.CreateModelMixin,
+#                     mixins.DestroyModelMixin,
+#                     viewsets.GenericViewSet):
+#     """>./"""
+#
+#     @action(methods=['get'],
+#             detail=False, )
+#     def subscriptions(self, request):
+#         """...0"""
+#
+#         following = User.objects.filter(following__user=self.request.user)
+#         serializer = FollowSerializer(following, many=True, context={'request': request})
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#
+#     @action(methods=['post', 'delete'], detail=True)
+#     def subscribe(self, request, pk):
+#         """../"""
+#
+#         following = get_object_or_404(User, id=pk)
+#         if request.method == 'POST':
+#             serializer = FollowSerializer(following, data=request.data, context={'request': request})
+#             if serializer.is_valid():
+#                 Follow.objects.create(user=request.user, following=following)
+#                 return Response(serializer.data,
+#                                 status=status.HTTP_201_CREATED)
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#         get_object_or_404(Follow, user=request.user, following=following).delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
