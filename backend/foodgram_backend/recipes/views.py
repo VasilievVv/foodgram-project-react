743451facbe3b15e227daf4022f9d9ff572144b7
VasilievVv <vvv141188@gmail.com>
2023-06-25 00:25:24 +0200

    add search filter ingredient

--- backend/foodgram_backend/recipes/views.py
+++ backend/foodgram_backend/recipes/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth import get_user_model
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, views, status, generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, views, status, generics, permissions, filters
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -30,6 +31,8 @@ class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
     permission_classes = [permissions.AllowAny]
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('name', )
 
 
 class FavoriteView(generics.CreateAPIView,
