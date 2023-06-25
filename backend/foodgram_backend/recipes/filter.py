7d113a1c4e2f1bd181e33d55dd9b701c51f13400
VasilievVv <vvv141188@gmail.com>
2023-06-25 15:01:33 +0200

    app filter recipe

--- backend/foodgram_backend/recipes/filter.py
+++ backend/foodgram_backend/recipes/filter.py
@@ -1,16 +1,34 @@
+from django.contrib.auth import get_user_model
 from django_filters import rest_framework
 
-from .models import Recipe
+from .models import Recipe, Tag
+
+User = get_user_model()
 
 
 class RecipeFilter(rest_framework.FilterSet):
     """Фильтр для модели Рецепта."""
 
-    # author =
+    author = rest_framework.ModelChoiceFilter(
+        queryset=User.objects.all(),
+        field_name='author',
+    )
     # is_favorited =
     # is_in_shopping_cart =
-    # tags =
+    tags = rest_framework.ModelChoiceFilter(
+        queryset=Tag.objects.all(),
+        field_name='tags__slug',
+    )
 
     class Meta:
         model = Recipe
-        fields = ('author', 'is_favorited', 'is_in_shopping_cart', 'tags', )
+        fields = ('author',
+                  # 'is_favorited',
+                  # 'is_in_shopping_cart',
+                  'tags', )
+
+    def get_is_favorited(self):
+        pass
+
+    def get_is_in_shopping_cart(self):
+        pass
